fb3f16d0349406694ba9d8bd75c7f1620ec70587
OmarElraies <omar.elraies@gmail.com>
2017-02-17 16:21:43 +0200

    Create media.py

---
+++ media.py
@@ -0,0 +1,18 @@
+class Movie(object):
+	"""A movie which contains alot of info that users interested in it
+
+    Attributes:
+        movie_title: the title of the movie.
+        movie_storyLine: a short breif for the movie.
+        poster_image: the url of poter of the movie.
+        trailer_youtube: the url youtube link  trailer of the movie.
+
+    """
+	def __init__(self, movie_title,movie_storyLine,
+				poster_image, trailer_youtube):
+		"""Return a new Movie object."""
+		self.title = movie_title
+		self.storyLine = movie_storyLine
+		self.poster_image_url = poster_image
+		self.trailer_youtube_url = trailer_youtube
+		
